e68b1871461059827e928adba504f8e68037c393
Sabri Charime <51125082+sabricharime@users.noreply.github.com>
2024-11-27 14:09:12 +0100

    Update vite.config.ts
    1

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,11 +11,6 @@ export default defineConfig({
   server:{
     proxy:{
       '/api/v1':"http://localhost:3000"
-    }
-  },
-  resolve:{
-    alias:{
-      '@':path.resolve(__direname,'src')
     }
   }
 })
